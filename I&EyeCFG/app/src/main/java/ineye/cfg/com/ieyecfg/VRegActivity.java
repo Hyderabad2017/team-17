d5b98ed9680fe3790258ce010ee3f91d7a2a5150
Swapna Gazula <swapnagazula@gmail.com>
2017-07-16 00:15:32 +0530

    added files

--- I&EyeCFG/app/src/main/java/ineye/cfg/com/ieyecfg/VRegActivity.java
+++ I&EyeCFG/app/src/main/java/ineye/cfg/com/ieyecfg/VRegActivity.java
@@ -26,17 +26,17 @@ import java.util.Map;
  */
 
 public class VRegActivity extends AppCompatActivity {
-    private EditText e_name,e_email,e_mobile,e_qual,e_loc,e_lang;
-    private String name,email,mobile,qual,loc,lang,gender;
+    private EditText e_name,e_email,e_mobile,e_qual,e_loc,e_lang,e_pass;
+    private String name,email,mobile,qual,loc,lang,gender,pass;
     private PrefManager prefManager;
     private RadioGroup r_gender;
     private RadioButton radiogender;
-    public static String url_reg="http://10.49.183.34/cfg_connection/vol_register.php";
+    public static String url_reg="http://192.168.43.96/cfg_connection/vol_register.php";
 
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_vreg);
 
         e_name=(EditText)findViewById(R.id.et_name);
         e_email=(EditText)findViewById(R.id.et_email);
@@ -44,11 +44,12 @@ public class VRegActivity extends AppCompatActivity {
         e_qual=(EditText)findViewById(R.id.et_qualification);
         e_loc=(EditText)findViewById(R.id.et_location);
         e_lang=(EditText)findViewById(R.id.et_language);
+        e_pass=(EditText)findViewById(R.id.et_password);
 
         r_gender=(RadioGroup)findViewById(R.id.rb_gender);
 
-        int selectedId=r_gender.getCheckedRadioButtonId();
-        radiogender=(RadioButton)findViewById(selectedId);
+        //int selectedId=r_gender.getCheckedRadioButtonId();
+       // radiogender=(RadioButton)findViewById(selectedId);
 
 
     }
@@ -62,13 +63,15 @@ public class VRegActivity extends AppCompatActivity {
         qual = e_qual.getText().toString();
         loc = e_loc.getText().toString();
         lang = e_lang.getText().toString();
-        gender=radiogender.getText().toString();
+        pass= e_pass.getText().toString();
+        gender="female";
 
         prefManager = new PrefManager(this);
 
         StringRequest strReq = new StringRequest(Request.Method.POST, url_reg, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
+                Toast.makeText(VRegActivity.this, response, Toast.LENGTH_LONG).show();
 
                 try {
                     JSONObject jObj = new JSONObject(response);
@@ -76,12 +79,15 @@ public class VRegActivity extends AppCompatActivity {
                     String value = jObj.getString("uid");
 
                     if (value.equalsIgnoreCase("success")) {
-
+                        Toast.makeText(VRegActivity.this, "Registration successful", Toast.LENGTH_SHORT).show();
                     }
                     else
                         Toast.makeText(VRegActivity.this, "Unexpected Error", Toast.LENGTH_SHORT).show();
 
                 } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                catch(Exception e){
                     e.printStackTrace();
                 }
 
@@ -100,12 +106,13 @@ public class VRegActivity extends AppCompatActivity {
             protected Map<String, String> getParams() {
                 Map<String, String> params = new HashMap<String, String>();
                 params.put("vol_name", name);
-                params.put("vol_pswd", email);
+                params.put("vol_email", email);
                 params.put("vol_gender", gender);
                 params.put("mobile", mobile);
                 params.put("city", loc);
                 params.put("language", lang);
                 params.put("qualification", qual);
+                params.put("vol_pswd", qual);
 
 
                 return params;
