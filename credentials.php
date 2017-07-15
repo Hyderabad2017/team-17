c4c8f9e665f15a792b6db8553ae45810c758a150
Dasarada Ram Reddy <dachi8958@gmail.com>
2017-07-15 19:00:38 +0530

    Update credentials.php

--- credentials.php
+++ credentials.php
@@ -7,5 +7,9 @@ $tb_events = "events";
 $tb_student = "student";
 $tb_volunteer = "volunteer";
 $dbconnected=mysqli_connect($hostname,$username,$password);
+if(!$dbconnected)
+{
+    die("Could not connect: " . mysql_error());
+}
 $dbselected=mysqli_select_db($dbconnected,$dbname);
 ?>
