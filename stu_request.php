a770609b29ed867d8f042f5ad04fcdd033c604a4
NaveenKumarReddy8 <mr.naveen8@gmail.com>
2017-07-16 04:05:08 +0530

    Update stu_request.php

--- stu_request.php
+++ stu_request.php
@@ -11,14 +11,14 @@ $stu_language="stu_language";
 $stu_email="stu_email";
 $req_status="pending";*/
 
-$stu_name=$_POST["stu_name"];
-$stu_exam=$_POST["stu_exam"];
-$exam_date=$_POST["exam_date"];
-$city=$_POST["location"];
-$stu_gender=$_POST["stu_gender"];
-$stu_mobile=$_POST["stu_mobile"];
-$stu_language=$_POST["stu_language"];
-$stu_email=$_POST["stu_email"];
+$stu_name=isset($_POST["stu_name"]);
+$stu_exam=isset($_POST["stu_exam"]);
+$exam_date=isset($_POST["exam_date"]);
+$city=isset($_POST["location"]);
+$stu_gender=isset($_POST["stu_gender"]);
+$stu_mobile=isset($_POST["stu_mobile"]);
+$stu_language=isset($_POST["stu_language"]);
+$stu_email=isset($_POST["stu_email"]);
 $req_status="pending";
 
 $sql1="INSERT into student(stu_name,stu_exam,stu_date_exam,stu_location,stu_gender,stu_mobile,stu_language,stu_email,stu_req_status) values('$stu_name','$stu_exam','$exam_date','$city','$stu_gender','$stu_mobile','$stu_language','$stu_email','$req_status')";
