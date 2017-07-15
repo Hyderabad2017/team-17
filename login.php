4013706b4d6091c30cafde815454baa990d65baf
NaveenKumarReddy8 <mr.naveen8@gmail.com>
2017-07-16 05:05:14 +0530

    Update login.php

--- login.php
+++ login.php
@@ -7,7 +7,7 @@ if($vol_mobile != NULL && $vol_pswd != NULL) {
 $sql = "SELECT vol_mobile, vol_pswd FROM cfg_17.volunteer WHERE vol_mobile = '$vol_mobile' and vol_pswd = '$vol_pswd'";
 
 $res = mysqli_query($dbconnected, $sql);
-if(mysqli_num_rows($res) == 1){
+if(mysqli_num_rows($res) >0){
   $list['uid']="success";
   echo json_encode($list);
 }
