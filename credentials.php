904895f70e75bd2d69bd55da684050fba887dfd6
Dasarada Ram Reddy <dachi8958@gmail.com>
2017-07-15 18:57:49 +0530

    Update Credentials.php

--- credentials.php
+++ credentials.php
@@ -6,4 +6,6 @@ $dbname = "cfg_17";
 $tb_events = "events";
 $tb_student = "student";
 $tb_volunteer = "volunteer";
- ?>
+$dbconnected=mysqli_connect($hostname,$username,$password);
+$dbselected=mysqli_select_db($dbconnected,$dbname);
+?>
