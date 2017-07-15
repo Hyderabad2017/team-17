b8231eaff5fd8fe7a71611ff10d75e7006994f8e
Swapna Gazula <swapnagazula@gmail.com>
2017-07-15 19:18:07 +0530

    changed vreg xml, working volunteer register

--- I&EyeCFG/app/src/main/java/ineye/cfg/com/ieyecfg/MainActivity.java
+++ I&EyeCFG/app/src/main/java/ineye/cfg/com/ieyecfg/MainActivity.java
@@ -33,64 +33,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        m_name = (EditText) findViewById(R.id.name);
-        m_name.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_FLAG_CAP_SENTENCES);
-        m_mail = (EditText) findViewById(R.id.email);
-        m_phone = (EditText) findViewById(R.id.phone);
+
     }
 
-    public void goToDetails(View view) {
-
-        String tag_string_req = "req_register";
-        name = m_name.getText().toString();
-        mail = m_mail.getText().toString();
-        phone = m_phone.getText().toString();
-        prefManager = new PrefManager(this);
-
-        StringRequest strReq = new StringRequest(Request.Method.POST, url_reg, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-
-                try {
-                    JSONObject jObj = new JSONObject(response);
-
-                    String value = jObj.getString("uid");
-
-                    if (value.equalsIgnoreCase("success")) {
-
-                    }
-                    else
-                        Toast.makeText(MainActivity.this, "Unexpected Error", Toast.LENGTH_SHORT).show();
-
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
 
 
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Toast.makeText(MainActivity.this, "Check your internet connection and proceed", Toast.LENGTH_SHORT).show();
-                //Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_LONG).show();
-            }
-        }
-
-        ) {
-            @Override
-            protected Map<String, String> getParams() {
-                Map<String, String> params = new HashMap<String, String>();
-                params.put("name", name);
-                params.put("mail", mail);
-                params.put("phone", phone);
-
-                return params;
-            }
-        };
-        AppController.getInstance().addToRequestQueue(strReq, tag_string_req);
-    }
-
-    
 
 
 }
---
+++ I&EyeCFG/app/src/main/java/ineye/cfg/com/ieyecfg/VRegActivity.java
@@ -0,0 +1,102 @@
+package ineye.cfg.com.ieyecfg;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.InputType;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by swapna on 15-07-2017.
+ */
+
+public class VRegActivity extends AppCompatActivity {
+    EditText e_name,e_email,e_mobile,e_qual,e_loc,e_lang;
+    String name,email,mobile,qual,loc,lang;
+    private PrefManager prefManager;
+    public static String url_reg="http://minimark.in/Android/register.php";
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        e_name=(EditText)findViewById(R.id.et_name);
+        e_email=(EditText)findViewById(R.id.et_email);
+        e_mobile=(EditText)findViewById(R.id.et_mobile);
+        e_qual=(EditText)findViewById(R.id.et_qualification);
+        e_loc=(EditText)findViewById(R.id.et_location);
+        e_lang=(EditText)findViewById(R.id.et_language);
+    }
+
+    public void goToRegMain(View view) {
+
+        String tag_string_req = "req_register";
+        name = e_name.getText().toString();
+        email = e_email.getText().toString();
+        mobile = e_mobile.getText().toString();
+        qual = e_qual.getText().toString();
+        loc = e_loc.getText().toString();
+        lang = e_lang.getText().toString();
+
+        prefManager = new PrefManager(this);
+
+        StringRequest strReq = new StringRequest(Request.Method.POST, url_reg, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+                try {
+                    JSONObject jObj = new JSONObject(response);
+
+                    String value = jObj.getString("uid");
+
+                    if (value.equalsIgnoreCase("success")) {
+
+                    }
+                    else
+                        Toast.makeText(VRegActivity.this, "Unexpected Error", Toast.LENGTH_SHORT).show();
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //Toast.makeText(MainActivity.this, "Check your internet connection and proceed", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        }
+
+        ) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("name", name);
+                params.put("mail", mail);
+                params.put("phone", phone);
+
+                return params;
+            }
+        };
+        AppController.getInstance().addToRequestQueue(strReq, tag_string_req);
+    }
+
+
+
+}
