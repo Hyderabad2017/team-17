47b66c4777f1603139742b9503afd9b681b3a338
Dasarada Ram Reddy <dachi8958@gmail.com>
2017-07-15 20:27:49 +0530

    Update vol_register.php

--- vol_register.php
+++ vol_register.php
@@ -9,7 +9,8 @@ include "credentials.php";
       $vol_city=$_POST["city"];
       $vol_email=$_POST["email"];
       $vol_lang=$_POST["language"];
-      
+      $vol_qualification=$_POST["qualification"];
+
       $sql1="SELECT * from volunteer where vol_mobile='$vol_mobile';";
       $res=mysqli_query($dbconnected,$sql1);
       if(mysqli_num_rows($res)>0)
@@ -19,11 +20,17 @@ include "credentials.php";
       }
       else
       {
-        $sql2="INSERT into volunteer(vol_name,vol_gender,vol_location,vol_mobile,vol_email,vol_language,vol_pswd) values('$vol_name','$vol_gender','$vol_city','$vol_mobile','$vol_email','$vol_lang','$vol_pswd');";
+        $sql2="INSERT into volunteer(vol_name,vol_gender,vol_location,vol_mobile,vol_email,vol_language,vol_pswd,vol_qualification) values('$vol_name','$vol_gender','$vol_city','$vol_mobile','$vol_email','$vol_lang','$vol_pswd','$vol_qualification');";
         $res1=mysqli_query($dbconnected,$sql2);
-        if($res1==TRUE){
-        $list['uid']="Success";
-        echo json_encode($list);
+        if($res1==TRUE)
+        {
+          $list['uid']="Success";
+          echo json_encode($list);}
+        else
+        {
+          $list['uid']="error";
+          echo json_encode($list);
+        }
       }
 
 
