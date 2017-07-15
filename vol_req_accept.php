b1e53da417ddd7b759a4318755aa2c535696f954
Dasarada Ram Reddy <dachi8958@gmail.com>
2017-07-16 01:13:06 +0530

    update accept_request

--- vol_req_accept.php
+++ vol_req_accept.php
@@ -10,10 +10,10 @@ $req_id=22;*/
 $sql1="SELECT req_status,primarys,secondary,backup from student where req_id='$req_id'";
 $res1=mysqli_query($dbconnected,$sql1);
 if($res1)
-{#echo "hi";
+{echo "hi";
   while($row = mysqli_fetch_assoc($res1))
   {
-    #echo "hi1";
+    echo "hi1";
     $req_status=$row['req_status'];
     $primarys=$row['primarys'];
     $secondary=$row['secondary'];
@@ -21,18 +21,18 @@ if($res1)
   }
   if($req_status=="pending")
   {
-    #echo "hi2";
+    echo "hi2";
     $req_status="active";
   }
   if($primarys==NULL)
   {
     $primarys=$vol_id;
   }
-  else if($secondary==NULL && $primary!=NULL)
+  else if($primarys!=0)
   {
     $secondary=$vol_id;
   }
-  else if($backup==NULL && $primary!=NULL && $secondary!=NULL)
+  else if($primarys!=0 && $secondary!=0)
   {
     $backup=$vol_id;
   }
@@ -48,7 +48,18 @@ if($res1)
     echo json_encode($list);
   }
   else {
-    $list['uid']="ierror";
+    $list['uid']="uerror";
+    echo json_encode($list);
+  }
+  $sql3="INSERT into events(req_id,vol_id) values('$req_id','$vol_id')";
+  $res3=mysqli_query($dbconnected,$sql3);
+  if($res3==TRUE)
+  {
+    $list['uid1']="success1";
+    echo json_encode($list);
+  }
+  else{
+    $list['uid1']="ierror";
     echo json_encode($list);
   }
 }
