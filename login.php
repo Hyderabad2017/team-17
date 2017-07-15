97d73e861c8586e66f54ac769b826a0304cc1c07
NaveenKumarReddy8 <mr.naveen8@gmail.com>
2017-07-15 23:32:29 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,8 +1,8 @@
 <?php
 {include('credentials.php');
 }
-$vol_mobile = 10;//$_POST['vol_mobile'];
-$vol_pswd = "abd";//$_POST['vol_pswd'];
+$vol_mobile = $_POST['vol_mobile'];
+$vol_pswd = $_POST['vol_pswd'];
 if($vol_mobile != NULL && $vol_pswd != NULL) {
 $sql = "SELECT vol_mobile, vol_pswd FROM cfg_17.volunteer WHERE vol_mobile = '$vol_mobile' and vol_pswd = '$vol_pswd'";
 
