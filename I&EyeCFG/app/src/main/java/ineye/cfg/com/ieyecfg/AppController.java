95df11cae9afc0809e10a184eea8ff88da092cb2
Swapna Gazula <swapnagazula@gmail.com>
2017-07-15 15:53:54 +0530

    added appcontroller

---
+++ I&EyeCFG/app/src/main/java/ineye/cfg/com/ieyecfg/AppController.java
@@ -0,0 +1,56 @@
+package ineye.cfg.com.ieyecfg;
+
+import android.text.TextUtils;
+import android.app.Application;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+/**
+ * Created by swapna on 15-07-2017.
+ */
+
+public class AppController extends Application{
+
+    public static final String TAG = AppController.class.getSimpleName();
+
+
+    private RequestQueue mRequestQueue;
+
+    private static AppController mInstance;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mInstance = this;
+    }
+
+    public static synchronized AppController getInstance() {
+        return mInstance;
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (mRequestQueue == null) {
+            mRequestQueue = Volley.newRequestQueue(getApplicationContext());
+        }
+
+        return mRequestQueue;
+    }
+
+    public <T> void addToRequestQueue(Request<T> req, String tag) {
+        req.setTag(TextUtils.isEmpty(tag) ? TAG : tag);
+        getRequestQueue().add(req);
+    }
+
+    public <T> void addToRequestQueue(Request<T> req) {
+        req.setTag(TAG);
+        getRequestQueue().add(req);
+    }
+
+    public void cancelPendingRequests(Object tag) {
+        if (mRequestQueue != null) {
+            mRequestQueue.cancelAll(tag);
+        }
+    }
+}
