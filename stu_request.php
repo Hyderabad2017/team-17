a599c0677b73b19de13a0aa1011e15154f411e3a
Dasarada Ram Reddy <dachi8958@gmail.com>
2017-07-16 05:30:27 +0530

    update student request

--- stu_request.php
+++ stu_request.php
@@ -21,7 +21,7 @@ $stu_mobile=$_POST["stu_mobile"];
 $stu_language=$_POST["stu_language"];
 $stu_email=$_POST["stu_email"];
 $req_status="pending";
-$sql1="INSERT into student(stu_name,stu_exam,date_exam,location,gender,mobile,language,email,req_status) values('$stu_name','$stu_exam','$exam_date','$city','$stu_gender','$stu_mobile','$stu_language','$stu_email','$req_status');";
+$sql1="INSERT into student(stu_name,stu_exam,stu_date_exam,stu_location,stu_gender,stu_mobile,stu_language,stu_email,stu_req_status) values('$stu_name','$stu_exam','$exam_date','$city','$stu_gender','$stu_mobile','$stu_language','$stu_email','$req_status');";
 $res=mysqli_query($dbconnected,$sql1);
 if($res===TRUE)
 {
